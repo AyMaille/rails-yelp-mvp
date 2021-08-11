ef1133236df346c4d8a48c6e2f4ca9d24631fa17
aymeric maille <aymeric.maille@laposte.net>
2021-08-11 17:06:06 +0200

    yelp on raise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create({
+                    name: 'Calay',
+                    address: 'rue Posay',
+                    category: 'french'
+                  })
+Restaurant.create({
+                    name: 'la belle frite',
+                    address: 'cours qui marche',
+                    category: 'belgian'
+                  })
+Restaurant.create({
+                    name: 'le sans sushi',
+                    address: 'barrière de pessac',
+                    category: 'chinese'
+                  })
+Restaurant.create({
+                    name: 'not chinese',
+                    address: 'barrière de toulouse',
+                    category: 'japanese'
+                  })
+Restaurant.create({
+                    name: 'Mario & Luigi',
+                    address: 'rue Denis',
+                    category: 'italian'
+                  })
+puts "gogogo"
